dfb41e0868959b424aad4c9eb02b70c31e64d48e
Matteda Ramesh <85834506+rummy143679@users.noreply.github.com>
2025-05-20 13:26:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+function  onClickSubmit(e) {
+	e.preventDefault();
+	console.log("click");
+	let first = document.getElementById("firstName");
+	let last = document.getElementById("lastName");
+	let phone = document.getElementById("phoneNumber");
+	let email = document.getElementById("emailId");
+	alert(`First Name: ${first.value} Last Name: ${last.value} Phone Number: ${phone.value} Email ID: ${email.value}`);
+}
